88ff9ce76a5043f315cc7cc19983326a75c0b504
りえ ちゃん <echann41@gmail.com>
2020-12-19 20:33:06 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  def index  # indexアクションを定義した
-    @posts = Post.all.order(id:"DESC")
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   end
 
   def checked
+    
     post = Post.find(params[:id])
     if post.checked 
       post.update(checked: false)
